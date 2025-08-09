d892134386494814c8c613409fa914170f4a4f9e
Demian <61812941+DemianAdam@users.noreply.github.com>
2025-08-09 10:58:06 -0300

    Update package.json, tsconfig.json, and vite.config.ts; add CNAME file for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,10 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import tailwindcss from '@tailwindcss/vite'
 
-// simulate __dirname in ESM
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  server: {
-    host: true,
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'cert/key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'cert/cert.pem')),
-    },
-    port: 5173,
-  },
+  plugins: [
+    react(),
+    tailwindcss()],
 })
